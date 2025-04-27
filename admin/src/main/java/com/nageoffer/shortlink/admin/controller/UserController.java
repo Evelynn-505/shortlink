caf60a414d8a0ad72c8b02f8e8e15f7ee6edebf7
Evelynn-505 <2115063252@qq.com>
2025-04-27 16:27:40 +0800

    feature: 开发短链接后管系统查询用户信息脱敏功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +26,15 @@ public class UserController {
     @GetMapping("/api/short-link/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
         return Results.success(userService.getUserByUsername(username));
+    }
+
+    /**
+     * 根据用户名查询无脱敏用户信息
+     * @param username
+     * @return
+     */
+    @GetMapping("/api/short-link/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -1,14 +1,14 @@
 package com.nageoffer.shortlink.admin.dto.resp;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.nageoffer.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
  * 用户返回参数响应
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
 @Data
-public class UserRespDTO {
+public class UserActualRespDTO {
+
     /**
      * id
      */
@@ -27,7 +27,6 @@ public class UserRespDTO {
     /**
      * 手机号
      */
-    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
