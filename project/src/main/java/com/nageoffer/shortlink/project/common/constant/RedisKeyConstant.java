57489ab59f74ea53921eafb9aef66dc7246a0efa
Evelynn-505 <2115063252@qq.com>
2025-07-11 16:34:28 +0800

    optimize:保持代码规范 将缓存key移至常量类

--- project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
@@ -38,4 +38,14 @@ public class RedisKeyConstant {
      * 短链接统计判断是否新IP缓存标识
      */
     public static final String SHORT_LINK_STATS_UIP_KEY = "short-link:stats:uip:";
+
+    /**
+     * 短链接监控消息保存队列 topic标识
+     */
+    public static final String SHORT_LINK_STATS_STREAM_TOPIC_KEY = "short-link:stats-stream:";
+
+    /**
+     * 短链接监控消息保存队列 group标识
+     */
+    public static final String SHORT_LINK_STATS_STREAM_GROUP_KEY = "short-link:stats-stream:only-group";
 }
